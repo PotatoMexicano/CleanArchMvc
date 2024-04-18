77bdeea78907b16c846dd3064d951912481d5ca1
Gabriel Cordeiro <potatomexicano006@yahoo.com>
2024-04-18 08:33:57 -0300

    [new] Adicionado soluções ao projeto base.
    Adicionado soluções ao projeto base.
    Domain, Application, Infra e Web.

---
+++ CleanArch.Domain/Entities/Category.cs
@@ -0,0 +1,32 @@
+﻿using CleanArch.Domain.Validation;
+
+namespace CleanArch.Domain.Entities
+{
+    public sealed class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public ICollection<Product> Products { get; set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            ValidateDomain(name);
+        }     
+        
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name.Name is required");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimun 3 caracters");
+
+            Name = name;
+        }
+    }
+}
