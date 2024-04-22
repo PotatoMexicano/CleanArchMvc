7f5d82842554a32fa824ef6d478c67e83be0dfe9
Gabriel Cordeiro <potatomexicano006@yahoo.com>
2024-04-22 08:41:11 -0300

    [feat] Adicionado interfaces de repository
    Adicionado interfaces de repositorio para Categoria e Produto.

---
+++ CleanArch.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArch.Domain.Entities;
+
+namespace CleanArch.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task<Category> GetByIdAsync(int id);
+
+        Task<Category> InsertAsync(Category category);
+        Task<Category> UpdateAsync(Category category);
+        Task<Category> DeleteAsync(Category category);
+    }
+}
