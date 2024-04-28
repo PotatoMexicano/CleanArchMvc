cd712314f74247c198b9125ac3cfbf2ea1ffb194
Potato Mexicano <potatomexicano006@yahoo.com>
2024-04-27 21:25:43 -0300

    [feat] Adicionado mapeamento & DTO
    Adicionado mapeamentos e DTO com validações para expor os modelos.
    Adicionado via automapper.

---
+++ CleanArch.Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArch.Application.DTOs
+{
+    public class CategoryDTO
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The name is required.")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+    }
+}
---
+++ CleanArch.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,40 @@
+﻿using CleanArch.Domain.Entities;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CleanArch.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The name is required.")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The description is required.")]
+        [MinLength(5)]
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "The price is required.")]
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "The stock is required.")]
+        [Range(1, 9999)]
+        public int Stock { get; set; }
+
+        [MaxLength(250)]
+        public string Image { get; set; }
+
+        public Category Category{ get; set; }
+        
+        [DisplayName("Categories")]
+        public int CategoryId { get; set; }
+    }
+}
