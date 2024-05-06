125324bdc28336bc331ab00db5e3fb402ec6c078
Gabriel Cordeiro <potatomexicano006@yahoo.com>
2024-05-06 08:20:02 -0300

    [feat] Adicionado View de produtos.
    Adicionado View de adição de produtos com Lista de Categories.

--- CleanArch.WebUI/Controllers/ProductsController.cs
+++ CleanArch.WebUI/Controllers/ProductsController.cs
@@ -1,17 +1,21 @@
 ﻿using CleanArch.Application.DTOs;
 using CleanArch.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CleanArch.WebUI.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productService, ICategoryService categoryService)
         {
             _productService = productService;
-        }
+            _categoryService = categoryService;
+
+		}
 
         [HttpGet]
         public async Task<IActionResult> Index()
@@ -19,5 +23,25 @@ namespace CleanArch.WebUI.Controllers
             IEnumerable<ProductDTO> products = await _productService.GetProducts();
             return View(products);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId = new SelectList(await _categoryService.GetCategories(), "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO product) 
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Add(product);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(product);
+        }
+
     }
 }
